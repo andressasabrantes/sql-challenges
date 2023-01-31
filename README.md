f87bc9d342928d40059872b88392f50fbb1c759b
Andressa Abrantes <andressasabrantes@gmail.com>
2023-01-31 20:40:03 -0300

    adding challenge 6

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ na California
 agrupados por tipo de cartao.
 
 5 - Preciso de uma lista com todas as pessoas que possuem o cartao de credito 'SuperiorCard'.
+
+6 - Este ano de 2022 foi excelente, e estamos muito felizes que a empresa atingiu suas metas. Por isso, queremos presentear alguns dos nossos funcionários mais antigos. Quero enviar um panetone ao endereço de cada um dos nossos engenheiros. Por isso, preciso visualizar o endereço completo de todos os engenheiros que começaram na empresa antes de 2022. Quero enviar junto com o presente, uma carta e um email agradecendo pelo tempo de trabalho e dedicação.
--- challenge6.sql
+++ challenge6.sql
@@ -1,7 +1,8 @@
-/* Este ano de 2022 foi excelente, e estamos muito felizes que a empresa atingiu suas metas. Por isso, queremos presentear alguns dos nossos funcionários mais antigos. 
-Quero enviar um panetone ao endereço de cada um dos nossos engenheiros.
-Por isso, preciso visualizar o endereço completo de todos os engenheiros que começaram na empresa antes de 2022.
-Quero enviar junto com o presente, uma carta agradecendo pelo tempo de trabalho e dedicação.*/
+/* Este ano de 2022 foi excelente, e estamos muito felizes que a empresa atingiu suas metas. 
+Por isso, queremos presentear alguns dos nossos funcionários mais antigos. 
+Quero enviar um panetone ao endereço de cada um dos nossos engenheiros. 
+Por isso, preciso visualizar o endereço completo de todos os engenheiros que começaram na empresa antes de 2022. 
+Quero enviar junto com o presente, uma carta e um email agradecendo pelo tempo de trabalho e dedicação.*/
 
 
 SELECT CONCAT(P.FirstName, ' ', P.LastName) AS FullName, PEMAIL.EmailAddress, HR.JobTitle, CONCAT(PA.AddressLine1, PA.AddressLine2,' ', PA.City, ' ', PA.StateProvinceID, ' ', PA.PostalCode) AS Adress, HR.HireDate
