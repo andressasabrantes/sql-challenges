ece3f6acaf16250dbc5d8156578a4ee39d36ed08
Andressa Abrantes <andressaabrantes@gmail.com>
2023-03-20 12:06:45 -0300

    Master: Procedure que mostra as pessoas que são de qualquer departamento

---
+++ challenge7.sql
@@ -0,0 +1,15 @@
+CREATE OR ALTER PROCEDURE departmentPeople(@nameDepartment AS VARCHAR(60))
+AS
+	BEGIN
+		SELECT CONCAT(P.FirstName, ' ', MiddleName, ' ', LastName) AS Full_Name, D.GroupName AS Departament_Name, D.DepartmentID AS Department_ID
+		FROM Person.Person P
+		JOIN HumanResources.EmployeeDepartmentHistory H
+			ON P.BusinessEntityID = H.BusinessEntityID
+		JOIN HumanResources.Department D
+			ON D.DepartmentID = H.DepartmentID
+		JOIN HumanResources.Employee E
+			ON E.BusinessEntityID = P.BusinessEntityID
+		WHERE D.GroupName = @nameDepartment
+	END
+
+EXEC departmentPeople @nameDepartment = 'Research and Development'
